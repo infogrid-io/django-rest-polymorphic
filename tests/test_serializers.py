jazzband/django-polymorphic

commit 4e65d533c4cd3d2405966ce244e196224f49fd95

Simple fix for #16 (#18)
This is a simple fix for #16. It adds the method `run_validation(data)` that performs the correct validation.
This new method is called on the `super()` call inside `is_valid()`

rest_polymorphic/serializers.py
@@ -4,6 +4,7 @@ from six import string_types
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 
 class PolymorphicSerializer(serializers.Serializer):
@@ -89,6 +90,14 @@ class PolymorphicSerializer(serializers.Serializer):
             child_valid = serializer.is_valid(*args, **kwargs)
             self._errors.update(serializer.errors)
         return valid and child_valid
+    
+    def run_validation(self, data=empty):
+        resource_type = self._get_resource_type_from_mapping(data)
+        serializer = self._get_serializer_from_resource_type(resource_type)
+        validated_data = serializer.run_validation(data)
+        validated_data[self.resource_type_field_name] = resource_type
+        return validated_data
+
 
     # --------------
     # Implementation

infogrid-io/django-rest-polymorphic

commit e5a01d517c8e404ab7ef45648692fc2c52e72a71

Fix serializer validation on partial update.
Prevents `TypeError` when using `partial=True`.

Regression in 4e65d533c4cd3d2405966ce244e196224f49fd95.

Issue raised in apirobot/django-rest-polymorphic#26.

Fix taken from apirobot/django-rest-polymorphic#31.

Co-authored-by: Lukas Burgey <lukas.burgey@kit.edu>

rest_polymorphic/serializers.py
@@ -91,6 +91,9 @@ class PolymorphicSerializer(serializers.Serializer):
         return valid and child_valid
 
     def run_validation(self, data=empty):
+        is_empty_value, data = self.validate_empty_values(data)
+        if is_empty_value:
+            return data
         resource_type = self._get_resource_type_from_mapping(data)
         serializer = self._get_serializer_from_resource_type(resource_type)
         validated_data = serializer.run_validation(data)

tests/models.py
@@ -22,3 +22,8 @@ class BlogThree(BlogBase):
 
     class Meta:
         unique_together = (('info', 'about'),)
+
+
+class Webpage(models.Model):
+    url = models.CharField(max_length=20)
+    blog = models.OneToOneField(BlogBase, on_delete=models.CASCADE)

tests/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from rest_polymorphic.serializers import PolymorphicSerializer
 
-from tests.models import BlogBase, BlogOne, BlogTwo, BlogThree
+from tests.models import BlogBase, BlogOne, BlogTwo, BlogThree, Webpage
 
 
 class BlogBaseSerializer(serializers.ModelSerializer):
@@ -40,3 +40,11 @@ class BlogPolymorphicSerializer(PolymorphicSerializer):
         BlogTwo: BlogTwoSerializer,
         BlogThree: BlogThreeSerializer
     }
+
+
+class WebpageSerializer(serializers.ModelSerializer):
+    blog = BlogPolymorphicSerializer()
+
+    class Meta:
+        model = Webpage
+        fields = ('url', 'blog')

tests/test_serializers.py
@@ -4,8 +4,8 @@ import pytest
 
 from rest_polymorphic.serializers import PolymorphicSerializer
 
-from tests.models import BlogBase, BlogOne, BlogTwo
-from tests.serializers import BlogPolymorphicSerializer
+from tests.models import BlogBase, BlogOne, BlogTwo, Webpage
+from tests.serializers import BlogPolymorphicSerializer, WebpageSerializer
 
 pytestmark = pytest.mark.django_db
 
@@ -153,3 +153,23 @@ class TestPolymorphicSerializer:
         err = duplicate.errors['non_field_errors']
 
         assert err == ['The fields info, about must make a unique set.']
+
+
+class TestNestedPolymorphicSerializer:
+
+    def test_partial_update(self):
+        # Partial update that does nothing to the child blog.
+
+        blog = BlogBase.objects.create(name='test-blog', slug='blog')
+        webpage = Webpage.objects.create(url='test-url', blog=blog)
+        data = {
+            'url': 'new-url',
+        }
+
+        serializer = WebpageSerializer(
+            webpage, data=data, partial=True
+        )
+        assert serializer.is_valid()
+
+        serializer.save()
+        assert webpage.url == 'new-url'